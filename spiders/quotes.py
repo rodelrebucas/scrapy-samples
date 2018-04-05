e8bd3038447240e7dd67d2cac58a2d400693003b
Rodel Rebucas <rodelrebucas@gmail.com>
2018-04-05 22:37:46 +0800

    initial commit

---
+++ spiders/quotes.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['toscrape.com']
+    start_urls = ['http://toscrape.com/']
+
+    def parse(self, response):
+        pass
