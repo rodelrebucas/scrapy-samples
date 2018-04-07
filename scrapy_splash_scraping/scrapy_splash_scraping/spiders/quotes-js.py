1959b18e0b649972412070edf67be198964d9d91
Rodel Rebucas <rodelrebucas@gmail.com>
2018-04-08 03:44:21 +0800

    add a scraping project that utilizes scrapy-splash

--- scrapy_splash_scraping/scrapy_splash_scraping/spiders/quotes-js.py
+++ scrapy_splash_scraping/scrapy_splash_scraping/spiders/quotes-js.py
@@ -1,17 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# a standalone spider
-# generated with:
-#   $scrapy genspider spider_name domain_name
-# run with:
-#   $scrapy runspider spider_name 
 import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['toscrape.com']
-    start_urls = ['http://quotes.toscrape.com']
+    name = 'quotesjs'
+    
 
     def parse(self, response):
         self.logger.info('Crawling this website: ' + response.url)
@@ -28,11 +20,3 @@ class QuotesSpider(scrapy.Spider):
                 'tag': quote.css('a.tag::text').extract()
             }   
             yield item
-
-        # following pagination
-        # joining next url with the base url
-        next_page = response.css('li.next > a::attr(href)').extract_first()
-        
-        if next_page:
-            next_page_url = response.urljoin(next_page)
-            yield scrapy.Request(url=next_page_url, callback=self.parse)
