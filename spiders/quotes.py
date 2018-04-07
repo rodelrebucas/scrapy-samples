64a1bb09b3617025bd27177d362e8234c7e5f224
Rodel Rebucas <rodelrebucas@gmail.com>
2018-04-08 02:06:27 +0800

    add parsing and author and handling next pages

--- spiders/quotes.py
+++ spiders/quotes.py
@@ -6,7 +6,7 @@
 # run with:
 #   $scrapy runspider spider_name 
 import scrapy
-
+from bs4 import BeautifulSoup, SoupStrainer
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -17,6 +17,14 @@ class QuotesSpider(scrapy.Spider):
         self.logger.info('Crawling this website: ' + response.url)
 
 
+        # obtaining listing details
+        # get the author details url
+        author_urls = response.css('.author + a::attr(href)').extract()
+        for url in author_urls
+            author_abs_url = response.joinurl(url)
+            yield scrapy.Request(url=url, callback=self.parse_details)
+
+        # Using css selectors
         # extract all quotes from the page
         # but yield each data individually
         for quote in response.css('div.quote'):
@@ -26,3 +34,11 @@ class QuotesSpider(scrapy.Spider):
                 'tag': quote.css('a.tag::text').extract()
             }   
             yield item
+
+        # following pagination
+        # joining next url with the base url
+        next_page = response.css('li.next > a::attr(href)').extract_first()
+        
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
