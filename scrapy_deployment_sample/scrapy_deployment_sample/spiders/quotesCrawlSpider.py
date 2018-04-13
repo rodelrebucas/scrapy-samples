1c8118334e25a281acd53fced051d49f699c9642
Rodel Rebucas <rodelrebucas@gmail.com>
2018-04-14 03:16:38 +0800

    linkextractor familiariazation

--- scrapy_deployment_sample/scrapy_deployment_sample/spiders/quotesCrawlSpider.py
+++ scrapy_deployment_sample/scrapy_deployment_sample/spiders/quotesCrawlSpider.py
@@ -14,14 +14,17 @@ class QuotesSpider(CrawlSpider):
     name = 'quotesCs'
     allowed_domains = ['toscrape.com', 'goodreads.com']
     login_url = ['http://quotes.toscrape.com/login']
-    start_urls = ['http://quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com','http://goodreads.com']
     rules = [
-        Rule(LxmlLinkExtractor(allow=(), process_value='external'), callback='parse_goodreads_author'),
+        Rule(LxmlLinkExtractor(allow=(r'/author/show/\d+\.[\w_]+')), callback='parse_goodreads_author'),
         # for every response extract this pattern
         # if this link is extracted, should we follow again and proceed with parsing the next response? True
-        # Rule(LxmlLinkExtractor(allow=(), restrict_xpaths=('//li[@class="next"]')), follow=True), 
+        Rule(LxmlLinkExtractor(allow=(), restrict_xpaths=('//li[@class="next"]')), follow=True), 
         # for every response extract this link pattern
-        # Rule(LxmlLinkExtractor(allow=(r'/author/[\w\.]+')), callback='parse_quotes_author')
+        # match only pattern on this domain, used if multiple start urls
+        # process_links are the links extracted from the  page
+        # process request for every page, must return request or None
+        Rule(LxmlLinkExtractor(allow=(r'/author/[\w\.]+'), allow_domains=['toscrape.com']), process_links='handler', process_request='handler', callback='parse_quotes_author')
     ]
 
 
@@ -41,22 +44,19 @@ class QuotesSpider(CrawlSpider):
         yield FormRequest.from_response(
             response,
             formdata=payload,
-            callback=super().start_request(response) 
+            callback=super().start_requests(response) 
             # Call the original start_request
             # to start processing start_urls 
         )
 
-    def external(value):
-        self.logger.info('***** -->> %s' % value)
-        
+    # change login to accomodate each caller
+    def handler(self, value):
+        try:
+            self.logger.info('***** -->> %s' % value)
+        except TypeError:
+            pass
 
     def parse_goodreads_author(self, response):
-        
-        self.logger.info('********************')
-
-        for link in LxmlLinkExtractor(allow=(r'http://goodreads.com/author/show/\d+\.[\w\_]+'),deny = self.allowed_domains).extract_links(response):
-            self.logger.info('********** %s' % link.url)
-
         author_name = response.xpath('//h1[@class="authorName"]/span/text()').extract_first()        
         yield {
             'author_name_goodreads': author_name
